434aea1db5c2b3a14d278a15b560c4230d1880bc
A00573901 <145053659+A00573901@users.noreply.github.com>
2023-09-14 12:56:44 -0600

    Create functionmachine.py

---
+++ functionmachine.py
@@ -0,0 +1,31 @@
+def f(x):
+  f_=-2*x+34
+  return f_x
+  
+y=f(10)
+
+print(y)
+
+def f(x):
+  f_x=-x/3-10
+  return f_X
+  
+y-f(10)
+
+print(y)
+
+def f(x):
+  f_x=-abs(3*x)
+  return f_x
+  
+y=f(10)
+
+printl(y)
+
+def f(x):
+  f_x=(x-2)**2
+  return f_x
+
+y=f(10)
+
+print(y)
